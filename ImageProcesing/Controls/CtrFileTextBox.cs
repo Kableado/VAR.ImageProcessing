f2bd626c4532e8b20cadfe873fcd4872a8253d56
Valeriano A.R <valen.alfonso@gmail.com>
2018-07-29 19:02:27 +0200

    Add utility controls (CtrFileTextBox, CtrImageView, CtrOutput)

---
+++ ImageProcesing/Controls/CtrFileTextBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ImageProcesing.Controls
+{
+    public class CtrFileTextBox : TextBox
+    {
+        public CtrFileTextBox()
+        {
+            DoubleClick += CtrFileTextBox_DoubleClick;
+        }
+
+        private void CtrFileTextBox_DoubleClick(object sender, EventArgs e)
+        {
+            FileDialog fileDialog = new OpenFileDialog();
+            DialogResult result = fileDialog.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                Text = fileDialog.FileName;
+            }
+        }
+    }
+}
