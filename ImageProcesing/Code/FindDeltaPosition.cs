437f52045e5f27bf42c4fd5a1c8e858b86f880b6
Valeriano A.R <valen.alfonso@gmail.com>
2018-07-31 01:32:58 +0200

    FindDeltaPosition: Basic algorithm using MeanSquareError as the error function.

---
+++ ImageProcesing/Code/FindDeltaPosition.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ImageProcesing.Code
+{
+    public class FindDeltaPosition
+    {
+        private readonly Image _imgStart;
+        private readonly Image _imgEnd;
+        private readonly int _width;
+        private readonly int _height;
+        private readonly byte[] _bytesStart;
+        private readonly byte[] _bytesEnd;
+
+        public FindDeltaPosition(Image imgStart, Image imgEnd)
+        {
+            _imgStart = imgStart;
+            _imgEnd = imgEnd;
+            _width = _imgStart.Width;
+            _height = _imgStart.Height;
+            _bytesStart = Image_GetPixelsBytesArray(_imgStart);
+            _bytesEnd = Image_GetPixelsBytesArray(_imgEnd);
+        }
+
+        private static byte[] Bitmap_GetPixelsBytesArray(Bitmap bmp)
+        {
+            Rectangle _imageRect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            int channels = Bitmap.GetPixelFormatSize(bmp.PixelFormat) / 8;
+            byte[] pixelData = new byte[bmp.Width * bmp.Height * channels];
+
+            BitmapData imageData = bmp.LockBits(
+                _imageRect,
+                ImageLockMode.ReadOnly,
+                bmp.PixelFormat);
+            Marshal.Copy(imageData.Scan0, pixelData, 0, pixelData.Length);
+            bmp.UnlockBits(imageData);
+
+            return pixelData;
+        }
+
+        private static byte[] Image_GetPixelsBytesArray(Image img)
+        {
+            Bitmap bmp = new Bitmap(img);
+            return Bitmap_GetPixelsBytesArray(bmp);
+        }
+
+        public double CalculateMeanSquareError(int offsetX = 0, int offsetY = 0)
+        {
+            long meanSquareRoot = 0;
+            int estimatedCount = ((_width - offsetX) * (_height - offsetY)) * 3;
+            for (int y = 0; y < _height; y++)
+            {
+                for (int x = 0; x < _width; x++)
+                {
+                    int x2 = x + offsetX;
+                    if (x2 < 0 || x2 >= _width) { continue; }
+                    int y2 = y + offsetY;
+                    if (y2 < 0 || y2 >= _height) { continue; }
+
+                    long offset1 = (x + (y * _width)) * 4;
+                    long offset2 = ((x + offsetX) + ((y + offsetY) * _width)) * 4;
+
+                    for (int c = 0; c < 3; c++)
+                    {
+                        int diff = _bytesStart[offset1 + c] - _bytesEnd[offset2 + c];
+                        meanSquareRoot += (diff * diff);
+                    }
+                }
+            }
+            return (meanSquareRoot / (double)estimatedCount);
+        }
+
+        public class OffsetPosition
+        {
+            public int OffsetX { get; set; }
+            public int OffsetY { get; set; }
+            public double MeanSquareError { get; set; }
+        }
+
+        public OffsetPosition SearchOnSpiral(float percentage = 1.0f, int skip = 0, int offset = 0)
+        {
+            int halfWidth = _width / 2;
+            int halfHeight = _height / 2;
+            int max = (int)(Math.Max(_width, _height) * percentage);
+            max *= max;
+            int x = 0;
+            int y = 0;
+            int dx = 0;
+            int dy = -1;
+            OffsetPosition bestOffsetPosition = null;
+            List<OffsetPosition> listBestOffsetPositions = new List<OffsetPosition>();
+
+            for (int i = 0; i < max; i++)
+            {
+                Application.DoEvents();
+                bool process = false;
+                if ((-halfWidth < x) && (x <= halfWidth) && (-halfHeight < y) && (y <= halfHeight))
+                {
+                    process = true;
+                    if (skip > 0 && ((i + offset) % skip) != 0)
+                    {
+                        process = false;
+                    }
+                }
+
+                if (process)
+                {
+                    double meanSquareError = CalculateMeanSquareError(x, y);
+                    if (bestOffsetPosition == null || meanSquareError < bestOffsetPosition.MeanSquareError)
+                    {
+                        bestOffsetPosition = new OffsetPosition { OffsetX = x, OffsetY = y, MeanSquareError = meanSquareError, };
+                        listBestOffsetPositions.Add(bestOffsetPosition);
+                    }
+                }
+
+                if (x == y || (x < 0 && x == -y) || (x > 0 && x == (1 - y)))
+                {
+                    int temp;
+                    temp = dx;
+                    dx = -dy;
+                    dy = temp;
+                }
+                x = x + dx;
+                y = y + dy;
+            }
+            return bestOffsetPosition;
+        }
+    }
+}
--- ImageProcesing/FrmFindDeltaPosition.cs
+++ ImageProcesing/FrmFindDeltaPosition.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ImageProcesing.Code;
+using System;
+using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Windows.Forms;
 
@@ -17,7 +20,7 @@ namespace ImageProcesing
             ctrImageViewer.ImageShow = null;
             ctrOutput.Clean();
         }
-
+        
         private void btnCalculate_Click(object sender, EventArgs e)
         {
             ctrOutput.Clean();
@@ -36,7 +39,27 @@ namespace ImageProcesing
                 Image imgStart = Image.FromFile(txtPathImageStart.Text);
                 Image imgEnd = Image.FromFile(txtPathImageEnd.Text);
                 ctrImageViewer.ImageShow = imgStart;
+                Application.DoEvents();
 
+                FindDeltaPosition findDeltaPosition = new FindDeltaPosition(imgStart, imgEnd);
+                Stopwatch sw = Stopwatch.StartNew();
+                FindDeltaPosition.OffsetPosition offsetPosition = findDeltaPosition.SearchOnSpiral(0.2f, 10);
+                sw.Stop();
+
+                ctrOutput.AddLine(string.Format("Best: [X: {0}, Y: {1}] (Error:{2})", 
+                    offsetPosition.OffsetX, offsetPosition.OffsetY, offsetPosition.MeanSquareError));
+                ctrOutput.AddLine(string.Format("Time {0} MS", sw.ElapsedMilliseconds));
+
+                Bitmap bmpResult = new Bitmap(imgStart.Width, imgStart.Height, PixelFormat.Format24bppRgb);
+                Graphics g = Graphics.FromImage(bmpResult);
+                g.DrawImage(imgEnd, 0, 0, imgEnd.Width, imgEnd.Height);
+                g.DrawImage(imgEnd, -offsetPosition.OffsetX, -offsetPosition.OffsetY, imgEnd.Width, imgEnd.Height);
+
+                string strFileEndProcessed = string.Format("{0}/{1}.offset{2}", 
+                    Path.GetDirectoryName(txtPathImageEnd.Text), 
+                    Path.GetFileNameWithoutExtension(txtPathImageEnd.Text), 
+                    Path.GetExtension(txtPathImageEnd.Text));
+                bmpResult.Save(strFileEndProcessed);
             }
             catch (Exception ex)
             {
@@ -47,4 +70,5 @@ namespace ImageProcesing
             }
         }
     }
+
 }
