dd17a62f254389d942f2826e63bd3915f8852228
Valeriano A.R <valen.alfonso@gmail.com>
2018-08-04 02:38:05 +0200

    Better file and directory helpers.

---
+++ VAR.ImageProcessing.Toolbox/Code/FileHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace VAR.ImageProcessing.Toolbox.Code
+{
+    public class FileHelper
+    {
+        public static List<string> GetImageFilesOnDirectory(string directoryPath)
+        {
+            List<string> imageFiles = new List<string>();
+            string[] files = Directory.GetFiles(directoryPath);
+            foreach (string file in files)
+            {
+                string fileExtension = Path.GetExtension(file.ToLower());
+                if (
+                    fileExtension != ".jpg" &&
+                    fileExtension != ".jpeg" &&
+                    fileExtension != ".png" &&
+                    fileExtension != ".bmp" &&
+                    fileExtension != ".gif" &&
+                    true
+                    )
+                { continue; }
+                imageFiles.Add(file);
+            }
+            imageFiles = imageFiles.OrderBy(x => x).ToList();
+            return imageFiles;
+        }
+    }
+}
--- VAR.ImageProcessing.Toolbox/Code/PathHelper.cs
+++ VAR.ImageProcessing.Toolbox/Code/PathHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 
 namespace VAR.ImageProcessing.Toolbox.Code
 {
@@ -6,12 +7,50 @@ namespace VAR.ImageProcessing.Toolbox.Code
     {
         public static string AddSuffixToFilePath(string filePath, string suffix)
         {
-            string strNewFileName = string.Format("{0}/{1}{3}{2}",
+            string strNewFileName = string.Format("{0}{1}{2}{3}{4}",
                 Path.GetDirectoryName(filePath),
+                Path.DirectorySeparatorChar,
                 Path.GetFileNameWithoutExtension(filePath),
-                Path.GetExtension(filePath),
-                suffix);
+                suffix,
+                Path.GetExtension(filePath));
             return strNewFileName;
         }
+
+        public static string AddDirectoryToFilePath(string filePath, string directory, bool checkAndCreate = false)
+        {
+            string path = string.Format("{0}{1}{2}{3}",
+                Path.GetDirectoryName(filePath),
+                Path.DirectorySeparatorChar,
+                directory,
+                Path.DirectorySeparatorChar);
+            if (checkAndCreate)
+            {
+                CreatePath(path);
+            }
+            string strNewFileName = string.Format("{0}{1}",
+                path,
+                Path.GetFileName(filePath));
+            return strNewFileName;
+        }
+        
+        public static void CreatePath(string path)
+        {
+            var dirInfo = new DirectoryInfo(Path.GetDirectoryName(path));
+            DirectoryInfo rootDirInfo = dirInfo;
+            var listPathDirs = new List<DirectoryInfo>();
+            while (rootDirInfo.Parent != null)
+            {
+                listPathDirs.Insert(0, rootDirInfo);
+                rootDirInfo = rootDirInfo.Parent;
+            }
+            foreach (DirectoryInfo auxDirInfo in listPathDirs)
+            {
+                if (!auxDirInfo.Exists)
+                {
+                    auxDirInfo.Create();
+                }
+            }
+        }
+
     }
 }
