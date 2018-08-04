512fda23c473e0e1d82e2bb897077a99fa6ace6f
Valeriano A.R <valen.alfonso@gmail.com>
2018-08-04 02:36:57 +0200

    ImageHelper.OffsetImage: Complete implementation to really offset the image.

--- VAR.ImageProcessing.Toolbox/Code/ImageHelper.cs
+++ VAR.ImageProcessing.Toolbox/Code/ImageHelper.cs
@@ -7,10 +7,31 @@ namespace VAR.ImageProcessing.Toolbox.Code
     {
         public static Bitmap OffsetImage(int offsetX, int offsetY, Image img)
         {
+            offsetX = offsetX % img.Width;
+            if (offsetX < 0)
+            {
+                offsetX += img.Width;
+            }
+            offsetY = offsetY % img.Height;
+            if (offsetY < 0)
+            {
+                offsetY += img.Height;
+            }
             Bitmap bmpResult = new Bitmap(img.Width, img.Height, PixelFormat.Format24bppRgb);
             Graphics g = Graphics.FromImage(bmpResult);
-            g.DrawImage(img, 0, 0, img.Width, img.Height);
             g.DrawImage(img, offsetX, offsetY, img.Width, img.Height);
+            if (offsetX > 0)
+            {
+                g.DrawImage(img, offsetX - img.Width, offsetY, img.Width, img.Height);
+            }
+            if (offsetY > 0)
+            {
+                g.DrawImage(img, offsetX, offsetY - img.Height, img.Width, img.Height);
+            }
+            if (offsetX > 0 && offsetY > 0)
+            {
+                g.DrawImage(img, offsetX - img.Width, offsetY - img.Height, img.Width, img.Height);
+            }
             return bmpResult;
         }
     }
